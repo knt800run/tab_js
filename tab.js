3d4a09123790df99b6e8903265580a736e68b633
knt800run <hknt1255@gmail.com>
2021-05-31 08:50:33 +0900

    Create tab.js

---
+++ tab.js
@@ -0,0 +1,47 @@
+// 即時関数(グローバル汚染を防ぐ)
+(() => {
+
+  // ここに命令を書く
+
+  //  変数名に$をつけると変数はDOM要素だよ
+  const $doc = document;
+  const $tab = $doc.getElementById('js-tab');
+  const $nav = $tab.querySelectorAll('[data-nav]');
+  const $content = $tab.querySelectorAll('[data-content]');
+  const ACTIVE_CLASS = 'is-active';
+  const navLen = $nav.length;
+
+  // 初期化
+  const init = () => {
+    $content[0].style.display = 'block';
+  };
+  init();
+
+  // クリックイベント
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    //クリックされたnavとそのdataを取得
+    const $this = e.target;
+    const targetVal = $this.dataset.nav;
+
+    //対象外のnav,  content全て一旦リセットする
+    for (let i=0; i < navLen; i++) {
+      $content[i].style.display = 'none';
+      $nav[i].classList.remove(ACTIVE_CLASS);
+    }
+
+    // 対象のコンテンツをアクティブ化する
+    $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
+    $nav[targetVal].classList.add(ACTIVE_CLASS);
+
+  };
+
+  // 全nav要素に対して関数を適用・発火
+  for (let i=0; i < navLen; i++) {
+    $nav[i].addEventListener('click', (e) => {
+      handleClick(e);
+    });
+  }
+
+})();
